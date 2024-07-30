85023ac0db34edc2dc4cbeda9f99dcb3dab3832e
Anna Vetyugova <anna.vetyugova@sabris.ru>
2024-07-30 23:55:53 +0300

    fix: fix host

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -139,6 +139,6 @@ class Api {
   }
   }
   // Замените на адрес вашего бэкенда
-  const api = new Api('https://api.anna.vetyugova.nomorepartiesco.ru/');
+  const api = new Api('https://api.anna.vetyugova.nomorepartiesco.ru');
 
   export default api;
